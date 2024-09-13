1c6ef90827b5cc05c59848a892f511cd4bf124cf
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-12 20:41:51 -0600

    Updated index.html with new font links and title, refactored App.jsx to use GlobalStyles and separate Button and Input components, removed index.css and created new Button component in ui/Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -6,37 +9,22 @@ const H1 = styled.h1`
   background-color: aquamarine;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: #4caf50;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
 const StyledApp = styled.div`
-  background-color: black;
   padding: 20px;
 `;
 
-export default function App() {
+function App() {
   return (
-    <StyledApp>
-      <H1>Hello world</H1>
-      <Button>Check in</Button>
-      <Button>Check out</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello world</H1>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
 
-      <Input type="number" placeholder="Number of guest" />
-    </StyledApp>
+        <Input type="number" placeholder="Number of guest" />
+      </StyledApp>
+    </>
   );
 }
+export default App;
