c66e3ec052ec7edac7c673b9686288b5d1e83add
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-13 09:38:39 -0600

    Refactor App component to wrap inner elements with additional Row components and add default props to Row component

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,23 @@ function App() {
       <GlobalStyles />
 
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The oak nest</Heading>
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button>Check in</Button>
-            <Button>Check out</Button>
-          </div>
-        </Row>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The oak nest</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
 
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guest" />
-            <Input type="number" placeholder="Number of guest" />
-          </form>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guest" />
+              <Input type="number" placeholder="Number of guest" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
