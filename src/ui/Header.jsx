7edcb9aba2c256a2861fd1e232372c02ae006322
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-13 13:31:58 -0600

    Added styled components to AppLayout, Header, and Sidebar components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,3 +1,11 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: #333;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
 export default function Header() {
-  return <header>Header</header>;
+  return <StyledHeader>Header</StyledHeader>;
 }
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,3 +1,11 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: aliceblue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
 export default function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return <StyledSidebar>Sidebar</StyledSidebar>;
 }
