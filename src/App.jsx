259024df441463d57b8d64830fcad43202a30dd1
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-13 09:15:35 -0600

    Replace custom H1 styled component with imported Heading component, add two new headings, and reorganize layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: aquamarine;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   padding: 20px;
@@ -18,10 +13,12 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello world</H1>
+        <Heading as="h1">The oak nest</Heading>
+        <Heading as="h2">Check in and out</Heading>
         <Button>Check in</Button>
         <Button>Check out</Button>
 
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guest" />
       </StyledApp>
     </>
