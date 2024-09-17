4cc8eb40b5966cc6f4c3040c9cc584ff81f90093
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-17 09:40:31 -0600

    Refactor AppLayout, Header, and Sidebar components to use styled components with updated styles and layout

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
-  background-color: aliceblue;
+  background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
+  grid-row: 1 / -1;
 `;
 
 export default function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
