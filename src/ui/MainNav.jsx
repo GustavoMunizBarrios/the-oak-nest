097518fd5d229a33e3947d96eafb71f8d0d887b8
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-17 09:55:51 -0600

    Removed default-user.jpg, logo-dark.png, and logo-light.png images. Modified MainNav.jsx to import NavLink, update Link to StyledNavLink, and add a default function implementation.

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const NavList = styled.ul`
@@ -6,7 +8,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +46,18 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+export default function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">Home</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Bookings</StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
