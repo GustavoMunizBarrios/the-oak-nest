63d139d231fd03d94f5465cf422b9ff238da4af0
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-17 22:30:33 -0600

    Added Supabase dependencies and API call to Cabins component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getcabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    getcabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://wiezaqxevgqussnagtlm.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"></img>
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wiezaqxevgqussnagtlm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZXphcXhldmdxdXNzbmFndGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2MDMyNjcsImV4cCI6MjA0MjE3OTI2N30.lM2CNFN9aflpJ4FE8IBQPCK72lJhxzTrAsjoMRjB4XA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
