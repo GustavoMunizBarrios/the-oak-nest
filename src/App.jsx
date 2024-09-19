7694025bdb998fdf9c0ddf141aa53407f8936ae2
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-19 09:56:55 -0600

    Update App component styles: changed backgroundColor to var(--color-grey-0) and added color property with value var(--color-grey-700)

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,8 @@ export default function App() {
             fontSize: "16px",
             maxWidth: "500px",
             padding: "16px 24px",
-            backgroundColor: "var(--color-grey-700)",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
           },
         }}
       />
