d661881735e1e1c36df8d5dce90eb9ca285699eb
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-19 09:29:03 -0600

    Added success and error alerts to cabin deletion mutation

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -50,8 +50,10 @@ export default function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      alert("Cabin successfully deleted");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
     },
+    onError: (err) => alert(err.message),
   });
 
   return (
