1fbc257108fb106ccbef5180c313176d85d94a05
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-27 09:32:14 -0600

    Refactor Cabins page to use AddCabin component instead of CreateCabinForm, and add Modal component

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+
+export default function AddCabin() {
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setShowForm((show) => !show)}>
+        Add new cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
+    </div>
+  );
+}
