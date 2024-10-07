5b75cb64320cb0d1378a8ca9a12e96f01a10726e
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-10-06 21:18:40 -0600

    Comment out styled components import and TableHeader definition

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-import styled from "styled-components";
+//import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 
-const TableHeader = styled.header`
+/* const TableHeader = styled.header`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
@@ -18,7 +18,7 @@ const TableHeader = styled.header`
   font-weight: 600;
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
-`;
+`; */
 
 export default function CabinTable() {
   const { isLoading, cabins } = useCabins();
