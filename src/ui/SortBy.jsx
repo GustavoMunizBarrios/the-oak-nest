d590af6029df5e72eca78a3527262c33b8ba4e09
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-10-09 11:15:27 -0600

    Refactor CabinTable component to add sorting functionality and update SortBy component to reflect selected sort option

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -5,11 +5,19 @@ import Select from "./Select";
 /* eslint-disable react/prop-types */
 export default function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
 
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
     setSearchParams(searchParams);
   }
 
-  return <Select options={options} type={"white"} onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      type={"white"}
+      value={sortBy}
+      onChange={handleChange}
+    />
+  );
 }
