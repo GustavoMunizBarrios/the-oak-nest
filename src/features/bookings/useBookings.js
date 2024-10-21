49d5205dc2d53e1eabd68064577d97a222536534
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-10-21 10:33:31 -0600

    Added useQueryClient and prefetching to useBookings hook

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,25 +1,31 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
-  //FILTER
+  //FILTER ----------------------
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+  // -----------------------------------
 
-  // SORT
+  // SORT -----------------------------------------
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
+  //--------------------------------------------------
 
-  //PAGINATION
+  //PAGINATION ------------------------------------
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  //------------------------------------------------
 
+  // QUERY --------------------------------------------------------
   const {
     isLoading,
     data: { data: bookings, count } = {}, // we need "{ }" at the end for prevent null data
@@ -28,6 +34,23 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page], // when the "filter" or "sortBy" changes then will re-fetch the data
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+  // ------------------------------------------------------------------------
+
+  // PRE-FETCHING -------------------------------------------------
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { isLoading, error, bookings, count };
 }
