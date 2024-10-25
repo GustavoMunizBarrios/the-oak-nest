67c9f1f703fb0cc486849f6ef0734ff190717fb3
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-10-25 13:09:18 -0600

    Added ProtectedRoute component and integrated it into App.jsx, also added getCurrentUser function to apiAuth.js

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,3 +1,10 @@
 export default function ProtectedRoute({ children }) {
+  // 1. Load the authenticated user
+
+  // 2. While loading, show a spinner
+
+  // 3. If there is NO authenticated user, redirect to the /login
+
+  // 4. If there is  user, render the app
   return children;
 }
