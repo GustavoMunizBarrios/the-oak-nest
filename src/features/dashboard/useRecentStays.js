e25b8c5285f4518f4b4c9fad9db6142de2d12d5e
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-01 20:27:32 -0600

    Created useRecentStays.js hook

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,27 @@
+/* This custom React hook is designed to fetch recent booking data from an API based on a specified number of days. The hook uses React Router for handling URL search parameters and React Query for managing asynchronous data fetching and caching. */
+
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { useQuery } from "@tanstack/react-query";
+
+export function useRecentBookings() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+  // "subDays" is utility function to subtract a specified number of days from a given date.
+  // subDays(current date, number of days that you want to substract)
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+  return { isLoading, stays, confirmedStays };
+}
