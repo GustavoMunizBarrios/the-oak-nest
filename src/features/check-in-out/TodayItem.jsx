4af85f83f72eae1b530c0e149f299af31db39aa9
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-05 11:35:41 -0600

    Updated CheckoutButton, TodayItem, and Flag components with new imports, styles, and functionality.

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
+import Tag from "../../ui/Tag";
+import Button from "../../ui/Button";
+import { Flag } from "../../ui/Flag";
+import { Link } from "react-router-dom";
+import CheckoutButton from "./CheckoutButton";
 
 const StyledTodayItem = styled.li`
   display: grid;
@@ -24,5 +29,29 @@ const Guest = styled.div`
 export default function TodayItem({ activity }) {
   const { id, status, guests, numNights } = activity;
 
-  return <StyledTodayItem>Item</StyledTodayItem>;
+  return (
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
+      {status === "checked-in" && <Tag type="blue">Departing</Tag>}
+
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.nationality}`} />
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights}</div>
+
+      {status === "unconfirmed" && (
+        <Button
+          type="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+
+      {status === "checked-in" && (
+        <CheckoutButton bookingId={id}></CheckoutButton>
+      )}
+    </StyledTodayItem>
+  );
 }
