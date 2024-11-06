175b52ce762bb10d89bdcfca85aa26dab4c87a05
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-06 17:36:30 -0600

    Updated supabase key to use environment variable VITE_SUPABASE_KEY

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://wiezaqxevgqussnagtlm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZXphcXhldmdxdXNzbmFndGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2MDMyNjcsImV4cCI6MjA0MjE3OTI2N30.lM2CNFN9aflpJ4FE8IBQPCK72lJhxzTrAsjoMRjB4XA";
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
