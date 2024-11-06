6806cb87886540ca701cebfeaaf72100879bd67e
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-05 20:22:02 -0600

    Removed and replaced logo-dark.png and logo-light.png files, updated Logo.jsx to use SVG files instead

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -6,14 +6,14 @@ const StyledLogo = styled.div`
 `;
 
 const Img = styled.img`
-  height: 9.6rem;
+  height: 19rem;
   width: auto;
 `;
 
 function Logo() {
   const { isDarkMode } = useDarkMode();
 
-  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
+  const src = isDarkMode ? "/the-oak-nest-dark.svg" : "/the-oak-nest-light.svg";
 
   return (
     <StyledLogo>
